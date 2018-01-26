865c10bedbdb222e532568d4673f86d49c54f4a8
Alexander Harris <harris.alexk@gmail.com>
2018-01-26 13:41:08 -0500

    update provider example to use id param

--- provider.js
+++ provider.js
@@ -1,10 +1,12 @@
-const fetch = require('node-fetch')
 const request = require('request')
 
 function Model (koop) {
   Model.prototype.getData = function(req, callback) {
-    const url = 'https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json'
+    const url = 'https://raw.githubusercontent.com/'
 
+    // pull id from req.pram
+    // const id = req.param.id
+    const id = 'johan/world.geo.json/master/countries.geo.json'
     request(url, (err, res, body) => {
       if (err) return callback(err)
       const json = JSON.parse(res.body)
@@ -17,7 +19,5 @@ module.exports = {
   name: 'sample',
   type: 'provider',
   version: '0.0.1',
-  disableIdParam: true,
   Model: Model,
-  disabledIdParam: false
 }
